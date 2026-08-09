a1c1d2a6d6308560d871dde95645a7bfaaf0ebda
tea <nellisimba812@gmail.com>
2026-08-09 18:34:55 +0300

    Update To-do list.py

--- To-do list.py
+++ To-do list.py
@@ -1,4 +1,26 @@
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description='To-do list')
+
+subparsers = parser.add_subparsers(dest='command')
+
+add_parser = subparsers.add_parser("add", help='Добавить задачу')
+add_parser.add_argument('text', type=str)
+
+edit_parser = subparsers.add_parser("edit", help='Редактировать задачу по номеру')
+edit_parser.add_argument('index', type=int)
+edit_parser.add_argument('text', type=str)
+
+list_parser = subparsers.add_parser("list", help='Показать все задачи')
+
+done_parser = subparsers.add_parser("done", help='Отметить выполненным по номеру')
+done_parser.add_argument('index', type=int)
+
+delete_parser = subparsers.add_parser("delete", help='Удалить задачу по номеру')
+delete_parser.add_argument('index', type=int)
+
+args = parser.parse_args()
 
 def load_todos():
     try:
@@ -13,76 +35,105 @@ def save_todos():
     with open('todos.json', 'w') as f:
         json.dump(todos, f)
 
-def add_task(): #Добавляет задачу
-    text = input("Input a task: ")
+def add_task(text): #Добавляет задачу
     task = {"text": text, "done": False}
     todos.append(task)
     save_todos()
     print('A task has been added')
 
-def edit_task(): #Редактирует задачу
+def edit_task(index, text): #Редактирует задачу
     try:
-        show_todos()
-        ind = int(input("Which task would you like to edit?: "))
-        todos[ind - 1]["text"] = input("Input a edit task: ")
+        todos[index - 1]["text"] = text
         save_todos()
         print('A task has been edited')
-    except (ValueError, IndexError):
-        print('Invalid command')
+    except IndexError:
+        print('The index does not exist')
 
 def show_todos():   #Показывает все задачи
     for i, task in enumerate(todos):
         print(f'{i+1}. {task["text"]}; Is done? {task["done"]}')
 
-def mark_done(): #Отмечает выполненным
+def mark_done(index): #Отмечает выполненным
     try:
-        show_todos()
-        ind = int(input("Select an option: "))
-        todos[ind - 1]["done"] = True
+        todos[index - 1]["done"] = True
         save_todos()
         print('A task has been marked done')
-    except (ValueError, IndexError):
-        print('Invalid command')
+    except IndexError:
+        print('The index does not exist')
 
-
-def delete_task():  #Удаляет задачу
+def delete_task(index):  #Удаляет задачу
     try:
-        show_todos()
-        ind = int(input("Select an option: "))
-        todos.pop(ind - 1)
+        todos.pop(index-1)
         save_todos()
         print('A task has been deleted')
-    except (ValueError, IndexError):
-        print('Invalid command')
+    except IndexError:
+        print('The index does not exist')
 
-while True:
-    print('|||||||')
-    print('1. Add a task')
-    print('2. Edit task')
-    print('3. Mark done')
-    print('4. Delete task')
-    print('5. Show todos')
-    print('6. Exit')
-    command = input('What would you like to do?: ')
+#Запуск через команды в терминале
+if args.command == "add":
+    add_task(text=args.text)
 
-    if command == '6':
-        print('Goodbye!')
-        break
+elif args.command == "edit":
+    edit_task(index=args.index, text=args.text)
 
-    elif command == '1':
-        add_task()
+elif args.command == "list":
+    show_todos()
 
-    elif command == '2':
-        edit_task()
+elif args.command == "done":
+    mark_done(index=args.index)
 
-    elif command == '3':
-        mark_done()
+elif args.command == "delete":
+    delete_task(index=args.index)
 
-    elif command == '4':
-        delete_task()
+#Запуск через меню
+elif args.command is None:
 
-    elif command == '5':
-        show_todos()
+    while True:
+        print('|||||||')
+        print('1. Add a task')
+        print('2. Edit task')
+        print('3. Mark done')
+        print('4. Delete task')
+        print('5. Show todos')
+        print('6. Exit')
+        command = input('What would you like to do?: ')
 
-    else:
-        print('Invalid command')
+        if command == '6':
+            print('Goodbye!')
+            break
+
+        elif command == '1':
+            try:
+                add_task(text=input("Input a task: "))
+            except IndexError:
+                print('Please enter a valid number')
+
+
+        elif command == '2':
+            try:
+                show_todos()
+                edit_task(index=int(input("Select an option: ")), text=input("Input a edit task: "))
+            except ValueError:
+                print('Please enter a valid number')
+
+        elif command == '3':
+            try:
+                show_todos()
+                mark_done(index=int(input("Select an option: ")))
+            except ValueError:
+                print('Please enter a valid number')
+
+        elif command == '4':
+            try:
+                show_todos()
+                delete_task(index=int(input("Select an option: ")))
+            except ValueError:
+                print('Please enter a valid number')
+
+        elif command == '5':
+            show_todos()
+
+        else:
+            print('Invalid command')
+
+
